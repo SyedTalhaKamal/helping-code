ea36377af90cf0307ebcbdb2c0cb6254ea8889b5
SyedTalhaKamal <syedtalhakamal22@gmail.com>
2021-04-16 09:45:05 +0500

    Updated File

--- image-change-onclick-slider/app.js
+++ image-change-onclick-slider/app.js
@@ -11,39 +11,39 @@
         cssEase: 'linear'
       });
     
-       // Slider | one-product-slider
-       $('.one-product__slider_navigation').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        arrows: true,
-        dots: false,
-        infinite: true,
-        asNavFor: '.one-product__slider',
-        focusOnSelect: true,
-        centerMode: false,
-        vertical: true,
-        prevArrow:
-          "<button type='button' class='slick-prev pull-left'><i class='fas fa-caret-up'></i></button>",
-        nextArrow:
-          "<button type='button' class='slick-next pull-right'><i class='fas fa-caret-down'></i></i></button>",
-        responsive: [
-          {
-            breakpoint: 1000,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              vertical: false,
-              arrows: false
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-              vertical: false,
-              arrows: false
-            }
-          }
-        ]
-      });
+  // Slider | one-product-slider
+  $('.one-product__slider_navigation').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    infinite: true,
+    asNavFor: '.one-product__slider',
+    focusOnSelect: true,
+    centerMode: false,
+    vertical: true,
+    prevArrow:
+      "<button type='button' class='slick-prev pull-left'><i class='fas fa-caret-up'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-next pull-right'><i class='fas fa-caret-down'></i></i></button>",
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          vertical: false,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          vertical: false,
+          arrows: false
+        }
+      }
+    ]
+  });
