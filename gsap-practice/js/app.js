d0326e743df72ea9ccb4fd6e7be28dcb638eb884
SyedTalhaKamal <syedtalhakamal22@gmail.com>
2021-10-07 16:39:58 +0500

    update on 7 oct 2021

--- gsap-practice/js/app.js
+++ gsap-practice/js/app.js
@@ -1,8 +1,8 @@
 gsap.registerPlugin(ScrollTrigger);
-// gsap.to(".ball", {scrollTrigger: ".ball",x:700, rotation:360, duration:5,yoyo:true,repeat:-1});
 gsap.to(".ball", {scrollTrigger: {
     trigger: ".ball",
     start: "top bottom",
     markers: "true",
     toggleActions: "restart pause resume pause"
-},x:700, rotation:360, duration:5});
+},x:700, rotation:360, duration:5,yoyo:true,repeat:-1});
+
