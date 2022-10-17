e377040c028fe4fde900df0b125be29190418a96
SyedTalhaKamal <syedtalhakamal22@gmail.com>
2022-10-17 20:39:34 +0500

    update on 17 Oct 2022

--- globalStructureOnline/nav.php
+++ globalStructureOnline/nav.php
@@ -3,38 +3,32 @@
     <div class="row">
       <div class="col-12">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="index.php">
-            <img src="images/logo.png" alt="" loading="lazy">
-          </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fas fa-bars"></i>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item <?php if ($pg == "Home") {
-                                    echo "active";
-                                  } else {
-                                    echo "";
-                                  } ?>">
-                <a href="home.php" class="nav-link">Dashboard</a>
-              </li>
-
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
+          <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+              <img src="images/logo.png" alt="" loading="lazy">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item <?php if ($pg == "Home") {
+                                      echo "active";
+                                    } else {
+                                      echo "";
+                                    } ?>">
+                  <a href="home.php" class="nav-link">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#_">Features</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#_">Pricing</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
-
       </div>
     </div>
   </div>
